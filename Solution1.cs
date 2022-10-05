e9aa14ec79ceaee0ed68a2340c7745dee5d609bc
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-10-05 18:17:20 +0700

    Up solution 1

---
+++ Solution1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlogrithmPractice
+{
+    public class Solution1
+    {
+        public static int MajorityElement(int[] nums)
+        {
+            int size = nums.Length / 2;
+            int[] Count = new int[1000];
+            int[] SigCount = new int[1000];
+            foreach (int i in nums)
+            {
+                if (i >= 0)
+                {
+                    Count[i]++;
+                    if (Count[i] > size)
+                    {
+                        return i;
+                    }
+                }
+                else
+                {
+                    SigCount[i*-1]++;
+                    if (SigCount[i * -1] > size)
+                    {
+                        return i;
+                    }
+                }
+                
+            }
+            return -1;
+        }
+    }
+}
