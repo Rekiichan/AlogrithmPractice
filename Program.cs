6518d555c93c88564db27bafd608e594bf03d9a9
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-10-05 13:25:19 +0700

    first

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+
+    internal class Program
+    {
+        public int Solve(int n, int k, int[] a)
+        {
+            for (int i = 0; i < k; i++)
+            {
+                if (a[i] == 1)
+                {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
